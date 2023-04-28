59bd3ee3a3e2dfc5be1dd7b533c75de898a04d4b
brainpower <brainpower@mailbox.org>
2023-04-29 01:54:39 +0200

    trying to fix windows static build

---
+++ .github/workflows/scripts/prepare-cross-env.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+QTV=${QT_MAJOR_VERSION:-6}
+
+# basic preparations
+pacman -Sy && \
+pacman -S archlinux-keyring --noconfirm --needed && \
+pacman -Su --noconfirm && \
+pacman-key --init && \
+pacman-key --populate
+
+# some basic utils
+pacman -S --noconfirm --needed make ninja git cmake binutils patch base-devel unzip python rsync zip
+
+# mingw builds
+echo "[ownstuff]" >> /etc/pacman.conf && \
+echo "Server = https://ftp.f3l.de/~martchus/\$repo/os/\$arch " >> /etc/pacman.conf && \
+echo "Server = https://martchus.no-ip.biz/repo/arch/\$repo/os/\$arch " >> /etc/pacman.conf && \
+pacman-key --recv-keys B9E36A7275FC61B464B67907E06FE8F53CDC6A4C && \
+pacman-key --finger    B9E36A7275FC61B464B67907E06FE8F53CDC6A4C && \
+pacman-key --lsign-key B9E36A7275FC61B464B67907E06FE8F53CDC6A4C && \
+pacman -Sy
+
+# Install core MingW packages
+pacman -S --noconfirm \
+  pev `# for peldd ` \
+  mingw-w64-binutils \
+  mingw-w64-crt \
+  mingw-w64-gcc \
+  mingw-w64-configure \
+  mingw-w64-headers \
+  mingw-w64-winpthreads \
+  mingw-w64-meson \
+  mingw-w64-cmake \
+  mingw-w64-extra-cmake-modules \
+  mingw-w64-fontconfig \
+  mingw-w64-freeglut \
+  mingw-w64-freetype2 \
+  mingw-w64-gettext \
+  nsis \
+  mingw-w64-pkg-config \
+  mingw-w64-tools \
+  mingw-w64-vulkan-headers
+
+# Install MingW Qt5 packages
+pacman -S --noconfirm \
+  mingw-w64-qt"${QTV}"-base-static \
+  mingw-w64-qt"${QTV}"-imageformats-static \
+  mingw-w64-qt"${QTV}"-svg-static \
+  mingw-w64-qt"${QTV}"-tools-static
+
